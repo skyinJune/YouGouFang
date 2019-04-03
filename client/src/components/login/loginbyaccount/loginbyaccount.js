58267745af766adf2ea3bd65624659059a7951f9
skyinJune <215933471@qq.com>
2019-04-04 02:07:00 +0800

    选择用户注册类别

--- client/src/components/login/loginbyaccount/loginbyaccount.js
+++ client/src/components/login/loginbyaccount/loginbyaccount.js
@@ -5,7 +5,13 @@ import './loginbyaccount.css'
 import { Redirect} from 'react-router-dom';
 import { login} from '../../../actions/index';
 
-class Login extends Component {
+/**
+ *  通过账户密码登录的组件页
+ *
+ * @class LoginByAccount
+ * @extends {Component}
+ */
+class LoginByAccount extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -20,56 +26,100 @@ class Login extends Component {
         this.onRedirectToRegister = this.onRedirectToRegister.bind(this);
     }
 
+    /**
+     *  所有输入框输入内容时调用的方法
+     *
+     * @param {*} value  string 输入框中的内容
+     * @param {*} type  string 要setState的值('account', 'password')
+     * @memberof LoginByAccount
+     */
     inputOnchange(value, type) {
         this.setState({[type]: value});
     }
 
+    /**
+     *  点击登录按钮时调用的方法
+     *
+     * @memberof LoginByAccount
+     */
     onSubmitClick() {
         this.props.login(this.state.account);
     }
 
+    /**
+     *  点击眼睛时调用的方法
+     *
+     * @memberof LoginByAccount
+     */
     toggleShowPassword() {
+        // 点击眼睛时切换密码的明文和非明文显示方式
         this.state.showPassword ? this.setState({showPassword: false})
             : this.setState({showPassword: true});
     }
 
+    /**
+     *  点击右上角注册时调用的方法
+     *
+     * @memberof LoginByAccount
+     */
     onRedirectToRegister() {
+        // 让 Redirect 渲染出来，直接跳转到注册页
         this.setState({redirectToRegister: true});
     }
 
     render() {
+        // 这个元素是通过 this.state.showPassword 来控制眼睛的显示icon，通过切换className来实现切换
         const passwordExtra = this.state.password
                 ? <i className={`iconfont passwordeye ${this.state.showPassword? 'icon-yanjing_xianshi_o' : 'icon-yanjing_yincang_o'}`}/>
                 : null;
+
         return (
             <div className="accountloginwrapper">
+
+                {/* 如果已登录直接跳转到首页 */}
                 {this.props.logStatus.isLogin? <Redirect to="/"/> : null}
+
+                {/* 如果点击了右上角的注册(redirectToRegister为true)直接跳注册页 */}
                 {this.state.redirectToRegister? <Redirect to="/register"/> : null}
+
+                {/* Header部分 */}
                 <div className="acountloginheader">
                     <Icon type="left" size="lg"/>
                     <i className="login_by_account_header_register iconfont icon-zhuce" onClick={this.onRedirectToRegister}>注册新账号</i>
                 </div>
+
+                {/* Title部分 */}
                 <div className="accountlogintitle">账号密码登录</div>
+
+                {/* 账号输入框 */}
                 <InputItem placeholder="请输入账号" clear="true" onChange={value=>this.inputOnchange(value, 'account')}>
                     <i className="iconfont icon-yonghutouxiang accountloginicon"></i>
                 </InputItem>
+
+                {/* 密码输入框 */}
                 <InputItem placeholder="请输入密码" type={this.state.showPassword? 'text':'password'} 
                            onChange={value=>this.inputOnchange(value, 'password')} extra={passwordExtra}
                            onExtraClick={this.toggleShowPassword}
                            >
                     <i className="iconfont icon-mima accountloginicon"></i>
                 </InputItem>
+
+                {/* 登录按钮 */}
                 <Button className="accountloginbutton" type="primary" onClick={this.onSubmitClick}>登录</Button>
             </div>
         )
     }
 }
 
+// 引入store中的state
 const mapStateToProps = (state) => {
     return {logStatus: state.login}
 }
 
+// 引入需要的action
 const actionCreater = { login};
-Login = connect( mapStateToProps, actionCreater)(Login);
 
-export default Login;
+// 把action和store一起通过props绑定到这个组件上
+LoginByAccount = connect( mapStateToProps, actionCreater)(LoginByAccount);
+
+export default LoginByAccount;
--- client/src/components/resgister/resgisterbyaccount/resgisterbyaccount.js
+++ client/src/components/resgister/resgisterbyaccount/resgisterbyaccount.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import './registergyaccount.css';
-import {Button, Icon, InputItem, Toast} from 'antd-mobile';
+import {Button, Icon, InputItem, Toast, Radio, List} from 'antd-mobile';
 import { connect } from 'react-redux';
 import { login} from '../../../actions/index';
 import { Redirect} from 'react-router-dom';
+import 'whatwg-fetch';
 
+const RadioItem = Radio.RadioItem;
+
+/**
+ * 通过账户注册的组件页
+ *
+ * @class ResgisterByAccount
+ * @extends {Component}
+ */
 class ResgisterByAccount extends Component {
     constructor(props) {
         super(props);
@@ -12,60 +21,120 @@ class ResgisterByAccount extends Component {
             account: '',
             password: '',
             confirmPassword: '',
-            showPassword: false
+            showPassword: false,
+            userType: 0
         };
         this.inputOnchange = this.inputOnchange.bind(this);
         this.onSubmitClick = this.onSubmitClick.bind(this);
         this.toggleShowPassword = this.toggleShowPassword.bind(this);
         this.confirmPasswordToast = this.confirmPasswordToast.bind(this);
+        this.switchUserType = this.switchUserType.bind(this);
     };
 
+
+    /**
+     * 所有输入框输入内容时调用的方法
+     *
+     * @param {*} value string 输入框中的内容
+     * @param {*} type  string 要setState的值('account', 'password', 'confirmPassword')
+     * @memberof ResgisterByAccount
+     */
     inputOnchange(value, type) {
         this.setState({[type]: value});
     }
 
+    /**
+     *  点击注册按钮时调用的方法
+     *
+     * @memberof ResgisterByAccount
+     */
     onSubmitClick() {
-        if(this.state.password === this.state.confirmPassword) {
-            Toast.success(`注册成功! ${this.state.account}同学你已登录!`, 2);
-            this.props.login(this.state.account);
+        // 判断密码和确认密码是否都存在，否则Toast提示
+        if(this.state.account && this.state.confirmPassword) {
+            // 判断密码和确认密码是否一致，否则Toast提示
+            if(this.state.password === this.state.confirmPassword) {
+                // 注册成功后Toast提示，并且直接使用当前账户登录
+                Toast.success(`注册成功! 新同学 ${this.state.account} 你好，已自动登录!`, 2);
+                this.props.login(this.state.account);
+            }
+            else {
+                Toast.fail('密码输入不一致，请确认后再输入！', 2);
+            }
         }
         else {
-            Toast.fail('密码输入不一致，请确认后再输入！', 2);
+            Toast.fail('密码呢？密码呢？', 2);
         }
     }
 
+    /**
+     *  点击眼睛时调用的方法
+     *
+     * @memberof ResgisterByAccount
+     */
     toggleShowPassword() {
+        // 点击眼睛时切换密码的明文和非明文显示方式
         this.state.showPassword ? this.setState({showPassword: false})
             : this.setState({showPassword: true});
     }
 
+    /**
+     *  点击确认密码中的icon时调用的方法
+     *
+     * @memberof ResgisterByAccount
+     */
     confirmPasswordToast() {
+        // 点击确认密码中的提示icon,如果密码和确认密码一致则提示可注册，否则提示密码不一致
         this.state.password === this.state.confirmPassword?
             Toast.success('密码输入完成，快去点击注册吧！',2)
             :Toast.fail('密码输入不一致，请确认后再输入！',2)
     }
 
+    switchUserType(value) {
+        this.setState({userType: value})
+    }
+
     render() {
+
+        // 这个元素是通过 this.state.showPassword 来控制眼睛的显示icon，通过切换className来实现切换
         const passwordExtra = this.state.password
                 ? <i className={`iconfont register_by_account_passwordeye ${this.state.showPassword? 'icon-yanjing_xianshi_o' : 'icon-yanjing_yincang_o'}`}/>
                 : null;
+        
+        // 这个元素是用来控制确认密码后面的Extra icon的显示样式的
         const confirmPasswordExtra = this.state.confirmPassword ? 
                 (this.state.confirmPassword === this.state.password ?
                     <i className="iconfont icon-wancheng register_by_account_confirmOK"/>
                     :<i className="iconfont icon-warningo register_by_account_confirmErr"/>
                 )
                 :null;
+
+        // 这个数组存储Radio中的信息
+        const radioInfo = [
+            { value: 0, label: '普通用户', extra: '享有查看浏览所有房源、发布房源的特权' },
+            { value: 1, label: '中介', extra: '享有查看浏览所有房源、发布房源、受理中介业务的特权' },
+            ];
+        
         return (
             <div className="register_by_account_wrapper">
+
+                {/* 如果已登录直接跳转到首页 */}
                 {this.props.logStatus.isLogin? <Redirect to="/"/> : null}
+
+                {/* Header部分 */}
                 <div className="register_by_account_headIcon">
                     <Icon type="left" size="lg"/>
                     <i className="iconfont icon-shouji register_by_account_phoneIcon">手机号注册</i>
                 </div>
+
+                {/* Title部分 */}
                 <div className='register_by_account_title'>注册新账号</div>
+
+                {/* 账号输入框 */}
                 <InputItem placeholder="请输入账号" clear="true" onChange={value=>this.inputOnchange(value, 'account')}>
                     <i className="iconfont icon-yonghutouxiang register_by_account_inputicon"></i>
                 </InputItem>
+
+                {/* 密码输入框 */}
                 <InputItem placeholder="请输入密码" 
                         type={this.state.showPassword? 'text':'password'} 
                         onChange={value=>this.inputOnchange(value, 'password')} 
@@ -74,7 +143,10 @@ class ResgisterByAccount extends Component {
                         >
                     <i className="iconfont icon-mima register_by_account_inputicon"></i>
                 </InputItem>
+
+                {/* 确认密码输入框 */}
                 {
+                    // 当密码不为空时显示确认密码输入框
                     this.state.password?
                     <InputItem placeholder="请再次输入密码" type='password'
                             onChange={value=>this.inputOnchange(value, 'confirmPassword')} 
@@ -85,17 +157,41 @@ class ResgisterByAccount extends Component {
                     </InputItem>
                     :null
                 }
-                <Button className="accountloginbutton" type="primary" onClick={this.onSubmitClick}>注册</Button>
+
+                {/* 注册类别选择 */}
+                <div className="register_by_account_switchType">
+                    {/* 用一个List包裹起来 */}
+                    <List renderHeader={() => '选择注册类别'}>
+                        {
+                            // 把radioInfo中的对象都循环展示出来
+                            radioInfo.map(i => (
+                                <RadioItem key={i.value} checked={this.state.userType === i.value} onChange={()=>this.switchUserType(i.value)}>
+                                    {i.label}
+
+                                    {/* 当该RadioItem被选中时展示extra信息 */}
+                                    {this.state.userType === i.value? <List.Item.Brief>{i.extra}</List.Item.Brief> :null}
+                                </RadioItem>
+                                ))
+                        }
+                    </List>
+                </div>
+
+                {/* 注册按钮 */}
+                <Button className="register_by_account_button" type="primary" onClick={this.onSubmitClick}>注册</Button>
             </div>
         )
     }
 }
 
+// 引入store中的state
 const mapStateToProps = (state) => {
     return {logStatus: state.login}
 }
 
+// 引入需要的action
 const actionCreater = { login};
+
+// 把action和store一起通过props绑定到这个组件上
 ResgisterByAccount = connect( mapStateToProps, actionCreater)(ResgisterByAccount);
 
 export default ResgisterByAccount;
