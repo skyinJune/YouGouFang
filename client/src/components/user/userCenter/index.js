6d910a6455630cb5628e2ab43dc203e1610992b4
skyinJune <215933471@qq.com>
2019-04-05 23:24:07 +0800

    usercenter page init

---
+++ client/src/components/user/userCenter/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import './index.css'
+
+class UserCenterIndex extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="usercenter_index_wrapper">
+                userCenterIndex
+            </div>
+        )
+    }
+}
+
+export default UserCenterIndex;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App';
 import Login from './components/login/loginbyaccount/loginbyaccount';
 import Register from './components/resgister/resgisterbyaccount/resgisterbyaccount';
+import UserCenter from './components/user/userCenter'
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -18,6 +19,7 @@ ReactDOM.render(
             <Route exact path='/' component={App}/>
             <Route path='/login' component={Login}/>
             <Route path='/register' component={Register}/>
+            <Route path='/usercenter' component={UserCenter}/>
         </BrowserRouter>
     </Provider>, 
     document.getElementById('root')
